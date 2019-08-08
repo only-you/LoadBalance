413b9be84ce841d9db973276c18ef1ba2a89a3a2
only-you <584490885@qq.com>
2019-08-08 20:25:24 +0800

    Update CustomerInfo.java

--- workspace-common/src/main/java/com/aliware/tianchi/comm/CustomerInfo.java
+++ workspace-common/src/main/java/com/aliware/tianchi/comm/CustomerInfo.java
@@ -22,9 +22,9 @@ public class CustomerInfo {
         this.env = env;
         this.providerAllThreads = (int) (providerThread * 0.9);
         if ("large".equals(env)) {
-            this.serverWeight = 20;
+            this.serverWeight = 15;
         } else if ("medium".equals(env)) {
-            this.serverWeight = 12;
+            this.serverWeight = 9;
         } else if ("small".equals(env)) {
             this.serverWeight = 2;
         } else {
